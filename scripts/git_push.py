55a146e5abf06b8a1ced4c8cd87c1933f4a55ee7
Maylon <2450854008@qq.com>
2023-09-12 14:26:14 +0800

    update

--- scripts/git_push.py
+++ scripts/git_push.py
@@ -1,12 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+__author__ = "Maylon"
+
 from git import Repo
 import os
-
 
 print("Start pushing...")
 print()
 
 cnt = 0
-dir_file = os.path.abspath('../') # code的文件位置，默认将其存放在根目录下
+dir_file = os.path.abspath('../')  # code的文件位置，默认将其存放在根目录下
 repo = Repo(dir_file)
 
 g = repo.git
