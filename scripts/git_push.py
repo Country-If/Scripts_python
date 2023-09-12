d8c84cdf17391cbc72f3c5b5c37b6cc56b1a3b56
Maylon <2450854008@qq.com>
2023-09-12 14:25:06 +0800

    Initial commit

---
+++ scripts/git_push.py
@@ -0,0 +1,24 @@
+from git import Repo
+import os
+
+
+print("Start pushing...")
+print()
+
+cnt = 0
+dir_file = os.path.abspath('../') # code的文件位置，默认将其存放在根目录下
+repo = Repo(dir_file)
+
+g = repo.git
+
+while True:
+    cnt += 1
+    try:
+        g.push()
+        print("Try " + str(cnt) + ": Successfully push!")
+        break
+    except Exception as e:
+        print(e)
+        print("Try " + str(cnt) + ": Push failed, retrying...")
+        print()
+        continue
